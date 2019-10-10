9a7583958a3f5b4cf9c4234d49c4572bf26c7032
Michael Schiumo <mschiumo18@gmail.com>
2019-10-10 16:37:03 -0400

    Done.

--- index.js
+++ index.js
@@ -11,67 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-// const titleCased = tutorials.map(function (tutorial) {
-//    return tutorial.toUpperCase();
-//  });
-
- // const titleCased = tutorials.map(tutorial) => {
- //   return tutorial.toUpperCase();
- // }
-
-
-
- // const titleCased = tutorial => {
- //   return tutorials.map(function(tutorial) {
- //     tutorial.toUpperCase();
- //   })
- // };
-//
-// const titleCased = tutorials.map(function(tutorial) {
-//   return tutorial.toUpperCase();
-// });
-
-
 
 const titleCased = tutorial => {
    return tutorials.map(line => {
-    // let capitalizedWords = tutorial.split(' ').map(word => {
-    //   word[0].toUpperCase() + word.slice(1);
     let words = line.split(' ');
     let capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
     return capitalizedWords.join(' ');
   });
 };
-
- 
- // const titleCased = () => {
- //   return tutorials
- // }
-// array2 = array2.map(function (e) {
-//     return e.toUpperCase()
-// });
-
-
-
-
- // let doubled = arr.map(num => {
- //     return num * 2;
- // });
-
-
- // var result = arr.map(person => ({ value: person.id, text: person.name }));
- // console.log(result)
-
-
-//
-//    const activatedRobots = robots.map(function (robot) {
-//     return Object.assign({}, robot, {
-//       modes: robot.modes * 2,
-//       isActivated: true,
-//     });
-//   });const activatedRobots = robots.map(function (robot) {
-//   return Object.assign({}, robot, {
-//     modes: robot.modes * 2,
-//     isActivated: true,
-//   });
-// });
