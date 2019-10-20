ccc667a9022ddd65991dc1cd22a71d9ecbbce1e9
Savannah C <sgcorrell@gmail.com>
2019-10-20 17:39:55 +0000

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(string => {
+    const text = string.split(" ")
+    const capitals = text.map(text => text.charAt(0).toUpperCase() + text.slice(1))
+    const newText = capitals.join(" ")
+    return newText
+  })
 }
